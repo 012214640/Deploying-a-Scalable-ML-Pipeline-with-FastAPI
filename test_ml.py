893214909183c0cf638ccf54d2ce324e09943be6
Daniel Stromsodt <dstr127@wgu.edu>
2026-02-16 18:26:32 +0000

    Add unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,51 @@
 import pytest
-# TODO: add necessary import
+import numpy as np
+from ml.model import compute_model_metrics, train_model, inference
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model():
     """
-    # add description for the first test
+    # Test that model has predict method and is able to generate predictions as intended
     """
-    # Your code here
+    X = np.array([[0,1],[1,0],[1,1],[0,0]])
+    y = np.array([0,1,1,0])
+
+    model = train_model(X, y)
+
+    assert hasattr(model, "predict")
+
+    preds = model.predict(X)
+    assert len(preds) == len(y)
+
     pass
 
+def test_compute_model():
+    """
+    # Verify that compute model metrics are within the expected value range
+    """
+    y = np.array([1,0,1,1])
+    preds = np.array([1,0,0,1])
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert precision >= 0 and precision <= 1
+    assert recall >= 0 and recall <= 1
+    assert fbeta >= 0 and fbeta <= 1
+
     pass
 
+def test_training_reproducibility():
+    """
+    # Ensure that random state gives the same results for different runs of the training model
+    """
+    X = [[0], [1], [0], [1]]
+    y = [0, 1, 0, 1]
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
+    model1 = train_model (X, y)
+    model2 = train_model (X, y)
+
+    pred1 = inference(model1, X)
+    pred2 = inference(model2, X)
+
+    assert (pred1 == pred2).all()
+
     pass
